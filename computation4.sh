8b14d8f22ea8e9eeca420e537a6b0b341b635e7c
karthik <nallaboluk@gmaiil.com>
2020-05-22 10:59:16 +0530

    adding computation4UC

---
+++ computation4.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+function arithmetic()
+{
+x=$1
+operator="$2"
+y=$3
+case $operator in 
+     "+")
+     z=$(($x+$y))
+     ;; 
+     "*")
+     z=$(($x*$y))
+     ;; 
+     "/")
+     z=$(($x/$y))
+     ;; 
+     "%")
+     z=$(($x%$y))
+     ;; 
+esac
+return $z
+}
+
+read -p "enter value of a" a
+read -p "enter value of b" b
+read -p "enter value of c" c
+
+arithmetic $a "%" $b
+result=$?
+arithmetic $result "+" $c
+result4=$?
