ac52c811bbc5de1a3c70d3bc4c4b7b7f039caa7f
karthik <nallaboluk@gmaiil.com>
2020-05-22 09:30:37 +0530

    adding computation1

---
+++ computation1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+function arithmetic()
+{
+x=$1
+operator="$2"
+y=$3
+case $operator in 
+     "+")
+     z=$(($x+$y))
+     ;; 
+     "*")
+     z=$(($x*$y))
+     ;; 
+     "/")
+     z=$(($x/$y))
+     ;; 
+     "%")
+     z=$(($x%$y))
+     ;; 
+esac
+return $z
+}
+
+read -p "enter value of a" a
+read -p "enter value of b" b
+read -p "enter value of c" c
+
+arithmetic $b "*" $c
+result=$?
+arithmetic $result "+" $a
+result1=$?
+
